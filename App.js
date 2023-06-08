9e2733cef7ac77f45257d1e7217b42ec692facf0
wannacry3030 <wannacry.bat@proton.me>
2023-06-08 14:31:18 -0300

    update App.js

--- App.js
+++ App.js
@@ -2,12 +2,31 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      {/* Today's task */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          {/* aqui é aonde as tarefas vao ficar */}
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#E8EAED",
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddinghorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {},
 });
