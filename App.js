c54a41db0e8dc942ba0c18c516ef1c73ba1420a4
wannacry3030 <wannacry.bat@proton.me>
2023-06-08 15:08:18 -0300

    update App.js

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
@@ -15,6 +22,18 @@ export default function App() {
           <Task text={"Task 2"} />
         </View>
       </View>
+      {/* escrevendo uma tarefa */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"Escreva uma tarefa"} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -35,4 +54,8 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {},
+  input: {},
+  addWrapper: {},
+  addText: {},
 });
